97cc0035fa5cc530ccf256edbb5b262d1bfd0562
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-10-12 17:47:08 +0200

    Home Page
    - Add Axios and Moment.js
    - Fetching data for all the books and displaying it

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,9 +4,9 @@ import NavBar from "./NavBar"
 
 const Layout = ({ children }: React.PropsWithChildren<{}>) => {
   return (
-    <div className="flex flex-col items-center  min-h-screen py-2">
+    <div className="flex flex-col items-center  min-h-screen py-2 ">
       <NavBar />
-      <main className="container flex  flex-col items-center justify-center w-full flex-1 text-center">
+      <main className="container flex  flex-col items-center justify-center w-full flex-1 text-center my-5">
         {children}
       </main>
       <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,73 @@
-import Head from "next/head"
 import type { NextPage } from "next"
+import Head from "next/head"
+import Link from "next/link"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import moment from "moment"
+
+const getID = (url: String) => url.split("/").pop()
+
+interface IBook {
+  name: string
+  released: string
+  isbn: string
+  url: string
+}
 
 const Home: NextPage = () => {
+  const [books, setBooks] = useState<Array<IBook>>()
+  const [loading, setLoading] = useState<Boolean>(true)
+  const [error, setError] = useState<String>("")
+
+  const getBooks = () => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API}/books?pageSize=15`)
+      .then((res) => {
+        let tempBooks: Array<IBook> = []
+        res.data.map((books: any) => {
+          tempBooks.push({
+            name: books.name,
+            released: books.released,
+            isbn: books.isbn,
+            url: books.url,
+          })
+        })
+        setLoading(false)
+        setBooks(tempBooks)
+      })
+      .catch((error) => {
+        setLoading(false)
+        setError(`Character List : ${error.message}`)
+      })
+  }
+
+  useEffect(() => {
+    getBooks()
+  }, [])
+
+  if (loading) return <div>Loading ...</div>
+
+  if (error) return <div>{error} ...</div>
+
   return (
     <div className="">
       <Head>
         <title>Home - GOT LIBRARY</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className=""> Home Page</div>
+      <div className="flex flex-wrap items-center justify-around max-w-4xl sm:w-full">
+        <h2 className="text-3xl font-bold mb-5">All Ice and Fire Books</h2>
+        {books &&
+          books.map((book) => (
+            <Link href={""} key={book.isbn}>
+              <a className="grid grid-cols-3  p-4 mt-2 w-full text-left border mx-2 rounded-xl hover:text-indigo-600 focus:text-indigo-600">
+                <div> {book.name} </div>
+                <div> {moment(book.released).format("Do MMMM YYYY")}</div>
+                <div> ISBN: {book.isbn}</div>
+              </a>
+            </Link>
+          ))}
+      </div>
     </div>
   )
 }
